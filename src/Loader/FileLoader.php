20a3c4018e0dcc8719626c251460cbcce824f4b7
rxwyun <1120309488@qq.com>
2023-03-23 11:02:47 +0800

    update composer.json

--- src/Loader/FileLoader.php
+++ src/Loader/FileLoader.php
@@ -12,65 +12,8 @@
 
 namespace W7\Lang\Loader;
 
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Str;
 use Illuminate\Translation\FileLoader as LaravelTranslationFileLoader;
 use W7\Contract\Translation\LoaderInterface;
 
 class FileLoader extends LaravelTranslationFileLoader implements LoaderInterface {
-	/**
-	 * @var array
-	 */
-	protected $paths;
-
-	/**
-	 * FileLoader constructor.
-	 * @param Filesystem $files
-	 * @param $path
-	 * @param array $paths
-	 */
-	public function __construct(Filesystem $files, $path, $paths = []) {
-		$this->paths = $paths;
-		parent::__construct($files, $path);
-	}
-
-	/**
-	 * Load the messages for the given locale.
-	 *
-	 * @param string $locale
-	 * @param string $group
-	 * @param string $namespace
-	 *
-	 * @return array
-	 */
-	public function load($locale, $group, $namespace = null) {
-		$defaults = [];
-		if ($group !== '*') {
-			foreach ($this->paths as $path) {
-				$defaults = array_replace_recursive($defaults, $this->loadPath($path, $locale, $group));
-			}
-		}
-
-		return array_replace_recursive($defaults, parent::load($locale, $group, $namespace));
-	}
-
-	/**
-	 * Fall back to base locale (i.e. de) if a countries specific locale (i.e. de-CH) is not available.
-	 *
-	 * @param string $path
-	 * @param string $locale
-	 * @param string $group
-	 *
-	 * @return array
-	 */
-	protected function loadPath($path, $locale, $group) {
-		if (!$path) {
-			return [];
-		}
-		$result = parent::loadPath($path, $locale, $group);
-		if (empty($result) && Str::contains($locale, '-')) {
-			return parent::loadPath($path, strstr($locale, '-', true), $group);
-		}
-		return $result;
-	}
 }
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -41,7 +41,7 @@ class ServiceProvider extends ProviderAbstract {
 				BASE_PATH . '/lang'
 			];
 
-			$loader = new FileLoader(new Filesystem(), BASE_PATH, $paths);
+			$loader = new FileLoader(new Filesystem(), $paths);
 			if (\is_callable([$loader, 'addJsonPath'])) {
 				$loader->addJsonPath(BASE_PATH . '/vendor/laravel-lang/lang/json/');
 				$loader->addJsonPath(BASE_PATH . '/lang/json/');
